47ca3f7700657e76cf94f9efa4d7e8c3d189d268
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2021-12-03 10:37:40 +0500

    day4

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,22 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import SpImage from "../assets/Supportimage.png"
 export default function Support() {
-    return (
-        <div>
-            <div className="row container">
-                <div className="col-6">
-                    <div>
-                        <div className="row"></div>
-                        <div className="supportCircle">
-                        <h3 className="supportHeading">
-                            Always On Support
-                        </h3></div>
-                        <hr></hr>
-                        <p>Lorem Ipsum is simply dummy text of the printing and
-                          typesetting industry...</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+        <div className="whyAce">
+            <center>
+                <h4>
+                    <span>
+                        Why Ace Cloud
+                    </span>
+                    <span className="hosting">
+                        Hosting?
+                    </span>
+                </h4>
+            </center>
         </div>
-    )
+      <div className="row container mx-5">
+        <div className="col-md-12 col-lg-6">
+          <div>
+            <div className="row"></div>
+            <div className="dot"></div>
+            <h3 className="supportHeading">Always On Support</h3>
+          </div>
+          <hr className="supportHr"></hr>
+          <p className="supportP">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
+          </p>
+        </div>
+        <div className="col-md-12 col-lg-6">
+        <img className="supportImage" src={SpImage}></img>
+        </div>
+      </div>
+    </div>
+  );
 }
